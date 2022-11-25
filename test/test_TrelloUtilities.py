58d2d64ac650d43f95d04e2bec48883835ce4e06
Patrick Freel <oriesta@gmail.com>
2022-11-25 11:09:23 -0700

    Summary: Resolved code review actions     Removed duplicate code     Added description of unit test
    Changes to be committed:
    	modified:   src/TrelloUtilities.py
    	modified:   test/test_TrelloUtilities.py

--- test/test_TrelloUtilities.py
+++ test/test_TrelloUtilities.py
@@ -3,6 +3,13 @@ sys.path.append('../src')
 import TrelloUtilities
 
 
+'''
+Function: test_getCredentials
+
+Purpose: This function is meant to be called by pytest and verifies the ability to 
+read in a Trello users public key and personal access token so that an http
+request can be sent to the Trello API.
+'''
 def test_getCredentials():
     credentials = TrelloUtilities.Credentials()
     assert credentials.key != ''
