6e6aadcc43b23ef7f601634139df45acd6ecdc9c
Patrick Freel <oriesta@gmail.com>
2022-12-01 18:42:17 -0700

    Summary: Fixed merge conflict
    Changes to be committed:
    	modified:   src/TrelloRequestFactory.py

--- src/TrelloRequestFactory.py
+++ src/TrelloRequestFactory.py
@@ -27,7 +27,6 @@ class TrelloRequestFactory():
         request = TrelloRequest(url=url, params=self.query, headers=self.headers)
         return request.get()
 
-<<<<<<< HEAD
     def getBoard(self, id):
 
         # GET /1/boards/{id}
@@ -35,8 +34,6 @@ class TrelloRequestFactory():
         request = TrelloRequest(url=url, params=self.query, headers=self.headers)
         return request.get()
 
-=======
->>>>>>> main
     def getBoardNames(self):
 
         url = f'{baseUrl}{memberRequest}/{self.settings.userId}/boards'
