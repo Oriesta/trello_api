9a9921249f2a90b22aa305fc6ea9cf144750e19e
Patrick Freel <oriesta@gmail.com>
2022-12-01 18:17:49 -0700

    Summary: Changed name unit test name from                to test_GetMember()    test_getMember()
    Changes to be committed:
    	modified:   test/test_TrelloMember.py

--- test/test_TrelloMember.py
+++ test/test_TrelloMember.py
@@ -6,7 +6,7 @@ Function: test_GetMember
 Purpose: This function is meant to be called by pytest and verifies the ability to 
 instantiate a Member object and initializes it from a Trello Member request
 '''
-def test_GetMember():
+def test_getMember():
     member = Member()
     assert member.initialized == True
     assert member.username == 'patrickfreel'
